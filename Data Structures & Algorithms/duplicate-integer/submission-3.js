849268884af38a0ca5e4403dccbc90d135f1ebb8
muralisamui <48375414+muralisamui@users.noreply.github.com>
2026-09-11 10:46:20 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+      let map = {};
+
+      for(let i=0; i<nums.length; i++){
+        if(map[nums[i]] === undefined){
+          map[nums[i]] = 1;
+        }else{
+          return true
+        }
+      }
+      return false
+    }
+}
